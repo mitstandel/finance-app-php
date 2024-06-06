73926dc645029397a5e477fac98b82f42bb09080
Mitesh Tandel <mits.tandel@gmail.com>
2024-06-06 22:51:04 +0530

    DB backups added.

---
+++ db-backups/dump-finance-20240620240606.sql
@@ -0,0 +1,137 @@
+-- MariaDB dump 10.17  Distrib 10.4.8-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: finance
+-- ------------------------------------------------------
+-- Server version	10.4.27-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_budget`
+--
+
+DROP TABLE IF EXISTS `tbl_budget`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_budget` (
+  `tbl_budget_id` int(11) NOT NULL,
+  `monthly_budget` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_budget`
+--
+
+LOCK TABLES `tbl_budget` WRITE;
+/*!40000 ALTER TABLE `tbl_budget` DISABLE KEYS */;
+INSERT INTO `tbl_budget` VALUES (1,10000);
+/*!40000 ALTER TABLE `tbl_budget` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_expense`
+--
+
+DROP TABLE IF EXISTS `tbl_expense`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_expense` (
+  `tbl_expense_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tbl_expense_category_id` int(11) NOT NULL,
+  `expense_name` text NOT NULL,
+  `expense_date` date NOT NULL,
+  `expense_spent` int(11) NOT NULL,
+  `expense_description` text NOT NULL,
+  PRIMARY KEY (`tbl_expense_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_expense`
+--
+
+LOCK TABLES `tbl_expense` WRITE;
+/*!40000 ALTER TABLE `tbl_expense` DISABLE KEYS */;
+INSERT INTO `tbl_expense` VALUES (1,1,'Lunch','2023-09-01',300,'Lunch on SM City'),(2,3,'Dinner','2023-09-01',100,'Aw'),(3,6,'Electricity','2023-09-02',1500,''),(4,5,'Bus','2023-09-02',50,'');
+/*!40000 ALTER TABLE `tbl_expense` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_expense_category`
+--
+
+DROP TABLE IF EXISTS `tbl_expense_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_expense_category` (
+  `tbl_expense_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` text NOT NULL,
+  `category_budget` int(11) NOT NULL,
+  PRIMARY KEY (`tbl_expense_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_expense_category`
+--
+
+LOCK TABLES `tbl_expense_category` WRITE;
+/*!40000 ALTER TABLE `tbl_expense_category` DISABLE KEYS */;
+INSERT INTO `tbl_expense_category` VALUES (1,'Allowance',800),(3,'Food',2000),(5,'Transportation',400),(6,'Utilities',2000);
+/*!40000 ALTER TABLE `tbl_expense_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_users`
+--
+
+DROP TABLE IF EXISTS `tbl_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(200) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `last_login_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+LOCK TABLES `tbl_users` WRITE;
+/*!40000 ALTER TABLE `tbl_users` DISABLE KEYS */;
+INSERT INTO `tbl_users` VALUES (1,'Admin','admin@email.com','09a1a0e07fc27a29df0724c04731613b3f0663f6','2024-06-06 22:09:34',NULL,'2024-06-06 22:27:10');
+/*!40000 ALTER TABLE `tbl_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'finance'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-06 22:49:53
